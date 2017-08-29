afd00ab3fa6f4b4b0bc5110b1e53bf641a973db7
Jorge Barbosa <jorangerbarbosa@gmail.com>
2017-08-29 07:22:49 -0400

    Auto stash before merge of "master" and "origin/master"

--- scripts/NetworkScan/DataBaseManagement.py
+++ scripts/NetworkScan/DataBaseManagement.py
@@ -6,7 +6,7 @@ dbPath = 'Scans.db'
 def InsertScan(gatewayIp, interface):
     conn = sqlite3.connect(dbPath)
     c = conn.cursor()
-    c.execute('INSERT INTO Scan(gatewayIp, interface, idTipoStatusScan, dataInicioScan) VALUES (?, ?, ?, ?)', (gatewayIp, interface, 1, str(datetime.now())))
+    c.execute('INSERT INTO SCAN(GatewayIp, Interface, IdTipoStatusScan, DataInicioScan) VALUES (?, ?, ?, ?)', (GatewayIp, Interface, 1, str(datetime.now())))
     conn.commit()
     conn.close()
     return c.lastrowid
@@ -14,7 +14,7 @@ def InsertScan(gatewayIp, interface):
 def InsertScanDispositivo(idScan, ip, mac, nomeFabricante, nomeOs):
     conn = sqlite3.connect(dbPath)
     c = conn.cursor()
-    c.execute('INSERT INTO ScanDispositivo(idScan, ip, mac, nomeFabricante, nomeOs) VALUES (?, ?, ?, ?, ?)', (idScan, ip, mac, nomeFabricante, nomeOs))
+    c.execute('INSERT INTO ScanDispositivo(IdScan, Ip, Mac, NomeFabricante, nomeOs) VALUES (?, ?, ?, ?, ?)', (IdScan, Ip, Mac, NomeFabricante, nomeOs))
     conn.commit()
     conn.close()
     return c.lastrowid
@@ -22,13 +22,13 @@ def InsertScanDispositivo(idScan, ip, mac, nomeFabricante, nomeOs):
 def InsertScanDipositivoPorta(idScanDispositivo, porta, nome):
     conn = sqlite3.connect(dbPath)
     c = conn.cursor()
-    c.execute('INSERT INTO ScanDispositivoPorta(idScanDispositivo, porta, nome) VALUES (?, ?, ?)', (idScanDispositivo, porta, nome))
+    c.execute('INSERT INTO ScanDispositivoPorta(IdScanDispositivo, Porta, Nome) VALUES (?, ?, ?)', (IdScanDispositivo, Porta, Nome))
     conn.commit()
     conn.close()
 
 def UpdateScanStatus(idScan, idTipoStatusScan):
     conn = sqlite3.connect(dbPath)
     c = conn.cursor()
-    c.execute('UPDATE Scan SET idTipoStatusScan = ? WHERE Id = ?', (idTipoStatusScan, idScan))
+    c.execute('UPDATE SCAN SET IdTipoStatusScan = ? WHERE Id = ?', (IdTipoStatusScan, IdScan))
     conn.commit()
     conn.close()
